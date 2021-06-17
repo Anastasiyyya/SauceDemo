67431049ec15655f848e0ebbec9e811fb1f77f1c
Anastasiya <anatasiya.ashevtsova888@gmail.com>
2021-06-17 17:56:08 +0300

    V1

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,13 +1,8 @@
 package pages;
 
 import constants.IConstants;
-import io.qameta.allure.Step;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 abstract class BasePage implements IConstants {
     WebDriver driver;
