caefb77645ed3317b0b1ee332a5e360b957bf8c3
Anastasiyyya <ashevtsova888@gmail.com>
2021-07-15 14:10:45 +0300

    fix

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -29,7 +29,7 @@ public class BaseTest implements IConstants {
     LoginSteps loginSteps;
 
     /**
-     * This method performed before the test methods
+     * This method performed before the test method
      * @param context
      */
     @BeforeMethod
